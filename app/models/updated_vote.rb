983f5236de61f134a0f4e2dec9a1f6b168bf8c89
Dinesh <dineshdhoom@gmail.com>
2012-02-07 00:53:47 +0530

    Added validations on dob field

--- app/models/updated_vote.rb
+++ app/models/updated_vote.rb
@@ -1,11 +1,12 @@
 class UpdatedVote < ActiveRecord::Base
-	validates :name, :middle_name, :last_name, :address_line_1, :presence => true
+	validates :name, :middle_name, :last_name, :sex, :presence => true
+	validate :date_of_birth_is_valid_datetime
 #	validate :date_of_birth_is_valid_datetime
 #	validates_date :date_of_birth, :on_or_before => lambda { Date.current }
 
 
   def date_of_birth_is_valid_datetime
-    errors.add(:date_of_birth, 'must be a valid date') if ((DateTime.parse(date_of_birth) rescue ArgumentError) == ArgumentError)
+    errors.add(:date_of_birth, 'can\'t be blank') if date_of_birth.first.blank? || date_of_birth.first.empty?
   end
 
 end
